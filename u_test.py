02fb3ccac659c85fead3e6fc6ee8e2da1805483a
Nagadurga79 <129742257+Nagadurga79@users.noreply.github.com>
2024-07-11 01:49:23 +0530

    file added

---
+++ u_test.py
@@ -0,0 +1,19 @@
+import math
+
+def test_sqrt():
+   num = 25
+   assert math.sqrt(num) == 5
+
+def testsquare():
+   num = 7
+   assert 7*7 == 40
+
+def testequality():
+   assert 11== 11
+   
+def test_sub(a,b):
+    
+    return a-b 
+
+def test_addition(a,b):
+    return a+b
